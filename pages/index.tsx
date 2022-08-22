decd43c8b407c83d2cf3f6a38187b311f3f69fba
himanshu <dewa.himanshu24@gmail.com>
2022-08-22 20:07:00 +0530

    Refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import type { NextPage } from 'next'
 import { Fragment } from 'react'
-import Login2 from './Login'
-
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import Login from './Login'
 
 const Home: NextPage = () => {
   return (
     <Fragment>
-      <Login2 />
+      <Login />
     </Fragment>
   )
 }
