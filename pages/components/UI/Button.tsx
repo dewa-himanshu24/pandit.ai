073550c0ecaef894ac29bcda88d986acbc881279
himanshu <dewa.himanshu24@gmail.com>
2022-08-26 00:32:24 +0530

    Remove comment

--- pages/components/UI/Button.tsx
+++ pages/components/UI/Button.tsx
@@ -8,9 +8,6 @@ const Button = (props: { classes: string; value: string;}) => {
         value={props.value}
         className="bordder-primary w-full cursor-pointer rounded-md border bg-primary py-3 px-5 text-base text-white transition duration-300 ease-in-out hover:shadow-md"
       />
-      {/* <button className="bordder-primary w-full cursor-pointer rounded-md border bg-primary py-3 px-5 text-base text-white transition duration-300 ease-in-out hover:shadow-md" onClick={props.onClickHandler}>
-        {props.value}
-      </button> */}
     </div>
   )
 }
