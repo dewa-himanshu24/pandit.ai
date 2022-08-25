c0da9ff8c2a42814d465c11042cc8c748500ccdc
himanshu <dewa.himanshu24@gmail.com>
2022-08-25 17:29:36 +0530

    Add pooja page

--- pages/PoojaCard.tsx
+++ pages/PoojaCard.tsx
@@ -1,17 +1,23 @@
+import Link from 'next/link';
 import React from 'react'
 
-const Pooja = (props: { imageUrl: string ; name: string }) => {
+
+const Pooja = (props: {
+  id: string; imageUrl: string ; name: string; description: string; 
+}) => {
+  
+
   return (
     <div className="w-full px-4 md:w-1/2 lg:w-1/3">
       <div className="wow fadeInUp group mb-10" data-wow-delay=".15s">
         <div className="mb-8 overflow-hidden rounded">
-          <a href="blog-details.html" className="block">
+          <Link href={`/pooja/${props.id}`} className="block">
             <img
               src={props.imageUrl}
               alt="image"
               className="w-full transition group-hover:rotate-6 group-hover:scale-125"
             />
-          </a>
+          </Link>
         </div>
         <div>
           <h3>
