2c87c87cc8b1395db9e7f109577bc89adc63cec5
himanshu <dewa.himanshu24@gmail.com>
2022-09-01 02:40:18 +0530

    Add headers

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 type Data = {
+  
   full_name: string | null |undefined,
   email: string | undefined,
 }
@@ -13,7 +14,8 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   if (req.method === 'GET') {
-    const bhakt = await prisma.bhakt.findFirst();
+    const xBhaktToken = req.headers['x-bhakt-token'];
+    const bhakt = await prisma.bhakt.findFirst({where: { id: Number(xBhaktToken) }});
     const userDetails = {
       full_name: bhakt?.full_name,
       email: bhakt?.email
