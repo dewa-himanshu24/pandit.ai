e29a6e9ca86ce14d88a83afe69df230abff68c30
himanshu <dewa.himanshu24@gmail.com>
2022-09-13 01:38:18 +0530

    Add register and login functionality

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,11 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const prisma = new PrismaClient()
 
 type Data = {
   message: string,
-  xBhaktToken: any
+  xBhaktToken?: any
+  decoded?: any
 }
 
 export default async function handler(
@@ -13,17 +16,36 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   if (req.method === 'POST') {
-    const password_hash = req.body.password.trim();
+    const password = req.body.password.trim();
     const email = req.body.email.trim().toLowerCase();
 
-    prisma.bhakt.findFirstOrThrow({ where: { email: email, password_hash: password_hash } })
-      .then(data => res.status(200).json({
-        message: "Successfully loggedin",
-        xBhaktToken: data.id
-      }))
+    prisma.bhakt.findFirstOrThrow({ where: { email: email } })
+      .then(data =>
+        bcrypt.compare(password, data.password_hash).then((match) => {
+          if (!match) {
+            res.status(400).json({
+              message: "Wrong Credential!",
+            })
+          }
+          const secret = "panditaijwttoken";
+
+          const payload = {
+            email: data.email,
+            id: data.id
+          }
+
+          const xBhaktToken = jwt.sign(payload, secret, {expiresIn: '1s'});
+          console.log(jwt.verify(xBhaktToken,secret));
+
+          res.status(200).json({
+            message: "Successfully loggedin",
+            xBhaktToken: xBhaktToken,
+            decoded: jwt.verify(xBhaktToken,secret)
+          });
+        }))
       .catch(err => res.status(400).json({
         message: "Something went wrong",
-        xBhaktToken: undefined
       }));
+
   }
 }
