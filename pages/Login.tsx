4c6c01c7db13680dd12341571eb39afdec3535a7
himanshu <dewa.himanshu24@gmail.com>
2022-09-14 17:13:54 +0530

    Integrate login page with login api and add check on login api

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getCookie, setCookie } from 'cookies-next';
+import { useRouter } from 'next/router';
 
 import Link from 'next/link'
 import Navbar from './Navbar'
 import Button from './components/UI/Button'
 
+// Wrong email or password - done
+// Redirect to wrong page - done
+// Redirect without token - done
+// Redirect with wrong token - pending
+// Redirect to Home page if right token present - done
+// Staty in login page if wrong token - present in cookie - pending
+
 const Login = () => {
+  const router = useRouter();
+
+  const loginBhakt = async (email: string, password: string) => {
+
+    const bhaktLoginInput = {
+      email, password
+    }
+
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify(bhaktLoginInput),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(response);
+
+    const responseJson = await response.json();
+    console.log(responseJson);
+    const token = responseJson.xBhaktToken;
+
+    if (response.status === 200) {
+      setCookie('xBhaktToken', token, { maxAge: 60 * 60 * 24 })
+      router.push('/HomePage');
+    }
+    else {
+      alert(responseJson.message)
+    }
+  }
+
+  const [enteredEmail, setEnteredEmail] = useState("")
+  const [enteredPassword, setEnteredPassword] = useState("")
+
+  // if cookie redirect to homepage.....
+  useEffect(() => {
+
+    if (getCookie('xBhaktToken')) {
+      router.push('/HomePage');
+    }
+
+  }, [])
+
+  const emailInputChangeHandler = (event: { target: { value: React.SetStateAction<string> } }) => {
+    setEnteredEmail(event.target.value);
+  };
+  const passwordInputChangeHandler = (event: { target: { value: React.SetStateAction<string> } }) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const formSubmmisionHandler = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    loginBhakt(enteredEmail, enteredPassword);
+
+    setEnteredEmail("");
+    setEnteredPassword("");
+  }
+
   return (
     <>
       <Navbar isLoggedIn={false} />
@@ -21,12 +87,14 @@ const Login = () => {
                     <p className="max-w-[160px]">Login</p>
                   </div>
                 </div>
-                <form>
+                <form onSubmit={formSubmmisionHandler}>
                   <div className="mb-6">
                     <input
                       type="email"
                       placeholder="Email"
                       className="bordder-[#E9EDF4] w-full rounded-md border bg-[#FCFDFE] py-3 px-5 text-base text-body-color placeholder-[#ACB6BE] outline-none transition focus:border-primary focus-visible:shadow-none"
+                      onChange={emailInputChangeHandler}
+                      value={enteredEmail}
                     />
                   </div>
                   <div className="mb-6">
@@ -34,6 +102,8 @@ const Login = () => {
                       type="password"
                       placeholder="Password"
                       className="bordder-[#E9EDF4] w-full rounded-md border bg-[#FCFDFE] py-3 px-5 text-base text-body-color placeholder-[#ACB6BE] outline-none transition focus:border-primary focus-visible:shadow-none"
+                      onChange={passwordInputChangeHandler}
+                      value={enteredPassword}
                     />
                   </div>
                   <Button value="Sign In" classes="" />
@@ -59,3 +129,7 @@ const Login = () => {
 }
 
 export default Login
+
+function setTokenCookie(token: any) {
+  throw new Error('Function not implemented.');
+}
