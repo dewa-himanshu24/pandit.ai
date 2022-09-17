9397254714bdec108bcb016b02f66f6d874c6d32
himanshu <dewa.himanshu24@gmail.com>
2022-09-18 02:29:29 +0530

    Remove dummy data

--- pages/api/pooja.ts
+++ pages/api/pooja.ts
@@ -3,58 +3,6 @@ import jwt, { JsonWebTokenError } from 'jsonwebtoken'
 import { PrismaClient } from '@prisma/client';
 import { PrismaClientInitializationError } from '@prisma/client/runtime';
 
-// const dummyPoojaList = {
-//   allPooja: [
-//     {
-//       id: 1,
-//       name: "Daily Aarti",
-//       imageUrl: "https://images.tv9hindi.com/wp-content/uploads/2022/05/aarti-rules.jpg?w=360"
-
-//     },
-//     {
-//       id: 2,
-//       name: "Radha Krishna Aarti",
-//       imageUrl: "https://c4.wallpaperflare.com/wallpaper/250/51/742/radha-krishna-radha-krishna-wallpaper-preview.jpg"
-//     },
-//     {
-//       id: 3,
-//       name: "Shiv Aarti",
-//       imageUrl: "https://i.ytimg.com/vi/MXDTEwxkU_I/maxresdefault.jpg"
-//     },
-//     {
-//       id: 4,
-//       name: "Hanuman Aarti",
-//       imageUrl: "https://images.herzindagi.info/image/2022/Apr/hanuman-jayanti-MAIN.jpg"
-
-//     },
-//     {
-//       id: 5,
-//       name: "Durga Aarti",
-//       imageUrl: "https://res.cloudinary.com/people-matters/image/upload/fl_immutable_cache,w_624,h_351,q_auto,f_auto/v1539759878/1539759877.jpg"
-//     },
-//     {
-//       id: 6,
-//       name: "Ganesha Aarti",
-//       imageUrl: "https://cdn.zeebiz.com/sites/default/files/styles/zeebiz_850x478/public/2022/08/30/197590-bappac5.jpg?itok=QgO45zhx"
-//     },
-//     {
-//       id: 7,
-//       name: "Vishnu Aarti",
-//       imageUrl: "https://www.bhaktiras.net/wp-content/uploads/2020/06/Shri-Vishnu-Mantra.png"
-
-//     },
-//     {
-//       id: 8,
-//       name: "Ram  Aarti",
-//       imageUrl: "https://www.digpu.com/wp-content/uploads/2021/04/Untitled-design-2021-04-21T100918.876.jpg"
-//     },
-//     {
-//       id: 9,
-//       name: "laxmi Aarti",
-//       imageUrl: "https://wallpaperaccess.com/full/1971315.png"
-//     }
-//   ]
-// }
 
 const prisma = new PrismaClient()
 
